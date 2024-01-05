6c8891d23ed449d59d68cea009bda3b66b52f943
John Lees <lees.john6@gmail.com>
2024-01-05 10:34:40 +0000

    Remove the text encoding package
    Don't think this is needed, looks like edge has it now

--- www/vue.config.js
+++ www/vue.config.js
@@ -5,7 +5,6 @@ const path = require("path");
 const { defineConfig } = require('@vue/cli-service')
 
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
-const webpack = require("webpack");
 module.exports = defineConfig({
     publicPath: '/vpm',
     transpileDependencies: true,
@@ -29,17 +28,6 @@ module.exports = defineConfig({
                     })
             )
             .end()
-            //  needed for Edge browser https://rustwasm.github.io/docs/wasm-bindgen/examples/hello-world.html
-            .plugin("text-encoder")
-            .use(webpack.ProvidePlugin)
-            .init(
-                (Plugin) =>
-                    new Plugin({
-                        TextDecoder: ["text-encoding", "TextDecoder"],
-                        TextEncoder: ["text-encoding", "TextEncoder"],
-                    })
-            )
-            .end();
         config.module.rule("js").exclude.add(/\.worker\.js$/);
         config.module
             .rule("worker")
